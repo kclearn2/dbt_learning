ac4539f798e1f70a8c412d8719431af9af7c0235
kylecalabria92@gmail.com <kylecalabria92@gmail.com>
2021-08-07 19:29:38 +0000

    packages

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -5,7 +5,7 @@ with payments as (
         orderid as order_id,
         paymentmethod as payment_method,
         status,
-        amount / 100 as amount,
+        {{ cents_to_dollars('amount', 4) }} as amount,
         created AS created_at,
         
     from dbt-tutorial.stripe.payment
